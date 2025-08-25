e981065638ece025e86e3c0cbd6bb9ab9e318477
Harshit <hargup3438@gmail.com>
2025-08-26 01:32:24 +0530

    add mock routes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -186,4 +187,225 @@ func main() {
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(port, mux))
+}
+
+type PlatformService interface {
+	SearchRoutes(req SearchRequest) ([]Route, error)
+	GetPlatformName() string
+}
+
+// RedBusService simulates RedBus API
+type RedBusService struct {
+	Name string
+}
+
+func (r *RedBusService) GetPlatformName() string {
+	return "RedBus"
+}
+
+func (r *RedBusService) SearchRoutes(req SearchRequest) ([]Route, error) {
+	// Simulate API delay
+	time.Sleep(time.Duration(rand.Intn(500)+200) * time.Millisecond)
+
+	locations := GetSampleLocations()
+	operators := GetSampleOperators()
+	busTypes := GetSampleBusTypes()
+
+	// Find from and to locations
+	var fromLoc, toLoc Location
+	for _, loc := range locations {
+		if loc.City == req.FromCity {
+			fromLoc = loc
+		}
+		if loc.City == req.ToCity {
+			toLoc = loc
+		}
+	}
+
+	// Generate mock routes
+	routes := []Route{}
+	basePrice := 500 + rand.Float64()*1000 // Random base price between 500-1500
+
+	for i := 0; i < rand.Intn(3)+2; i++ { // 2-4 routes
+		route := Route{
+			ID:            fmt.Sprintf("redbus_%d", i+1),
+			From:          fromLoc,
+			To:            toLoc,
+			Operator:      operators[0], // RedBus operator
+			BusType:       busTypes[rand.Intn(len(busTypes))],
+			DepartureTime: req.Date.Add(time.Hour * time.Duration(6+i*4)),   // 6AM, 10AM, 2PM, 6PM
+			ArrivalTime:   req.Date.Add(time.Hour * time.Duration(6+i*4+8)), // +8 hours journey
+			Duration:      "8h 0m",
+			Price: Price{
+				Amount:   basePrice + float64(i*100),
+				Currency: "INR",
+				Platform: "RedBus",
+			},
+			AvailableSeats: rand.Intn(20) + 5, // 5-25 seats
+			BookingURL:     "https://redbus.in/book/route123",
+		}
+		routes = append(routes, route)
+	}
+
+	return routes, nil
+}
+
+// MakeMyTripService simulates MakeMyTrip API
+type MakeMyTripService struct {
+	Name string
+}
+
+func (m *MakeMyTripService) GetPlatformName() string {
+	return "MakeMyTrip"
+}
+
+func (m *MakeMyTripService) SearchRoutes(req SearchRequest) ([]Route, error) {
+	time.Sleep(time.Duration(rand.Intn(600)+300) * time.Millisecond)
+
+	locations := GetSampleLocations()
+	operators := GetSampleOperators()
+	busTypes := GetSampleBusTypes()
+
+	var fromLoc, toLoc Location
+	for _, loc := range locations {
+		if loc.City == req.FromCity {
+			fromLoc = loc
+		}
+		if loc.City == req.ToCity {
+			toLoc = loc
+		}
+	}
+
+	routes := []Route{}
+	basePrice := 450 + rand.Float64()*1200
+
+	for i := 0; i < rand.Intn(4)+1; i++ { // 1-4 routes
+		route := Route{
+			ID:            fmt.Sprintf("mmt_%d", i+1),
+			From:          fromLoc,
+			To:            toLoc,
+			Operator:      operators[1], // MakeMyTrip operator
+			BusType:       busTypes[rand.Intn(len(busTypes))],
+			DepartureTime: req.Date.Add(time.Hour * time.Duration(7+i*3)),
+			ArrivalTime:   req.Date.Add(time.Hour * time.Duration(7+i*3+9)),
+			Duration:      "9h 0m",
+			Price: Price{
+				Amount:   basePrice + float64(i*150),
+				Currency: "INR",
+				Platform: "MakeMyTrip",
+			},
+			AvailableSeats: rand.Intn(15) + 3,
+			BookingURL:     "https://makemytrip.com/bus/book/xyz",
+		}
+		routes = append(routes, route)
+	}
+
+	return routes, nil
+}
+
+// GoibiboService simulates Goibibo API
+type GoibiboService struct {
+	Name string
+}
+
+func (g *GoibiboService) GetPlatformName() string {
+	return "Goibibo"
+}
+
+func (g *GoibiboService) SearchRoutes(req SearchRequest) ([]Route, error) {
+	time.Sleep(time.Duration(rand.Intn(400)+250) * time.Millisecond)
+
+	locations := GetSampleLocations()
+	operators := GetSampleOperators()
+	busTypes := GetSampleBusTypes()
+
+	var fromLoc, toLoc Location
+	for _, loc := range locations {
+		if loc.City == req.FromCity {
+			fromLoc = loc
+		}
+		if loc.City == req.ToCity {
+			toLoc = loc
+		}
+	}
+
+	routes := []Route{}
+	basePrice := 600 + rand.Float64()*900
+
+	for i := 0; i < rand.Intn(3)+2; i++ { // 2-4 routes
+		route := Route{
+			ID:            fmt.Sprintf("goibibo_%d", i+1),
+			From:          fromLoc,
+			To:            toLoc,
+			Operator:      operators[2], // Goibibo operator
+			BusType:       busTypes[rand.Intn(len(busTypes))],
+			DepartureTime: req.Date.Add(time.Hour * time.Duration(8+i*4)),
+			ArrivalTime:   req.Date.Add(time.Hour * time.Duration(8+i*4+7)),
+			Duration:      "7h 30m",
+			Price: Price{
+				Amount:   basePrice + float64(i*80),
+				Currency: "INR",
+				Platform: "Goibibo",
+			},
+			AvailableSeats: rand.Intn(25) + 8,
+			BookingURL:     "https://goibibo.com/bus/booking/abc",
+		}
+		routes = append(routes, route)
+	}
+
+	return routes, nil
+}
+
+// PlatformManager manages all booking platforms
+type PlatformManager struct {
+	platforms []PlatformService
+}
+
+func NewPlatformManager() *PlatformManager {
+	return &PlatformManager{
+		platforms: []PlatformService{
+			&RedBusService{Name: "RedBus"},
+			&MakeMyTripService{Name: "MakeMyTrip"},
+			&GoibiboService{Name: "Goibibo"},
+		},
+	}
+}
+
+func (pm *PlatformManager) SearchAllPlatforms(req SearchRequest) ([]Route, error) {
+	var allRoutes []Route
+
+	// Channel to collect routes from all platforms
+	routesChan := make(chan []Route, len(pm.platforms))
+	errorsChan := make(chan error, len(pm.platforms))
+
+	// Search all platforms concurrently
+	for _, platform := range pm.platforms {
+		go func(p PlatformService) {
+			routes, err := p.SearchRoutes(req)
+			if err != nil {
+				errorsChan <- fmt.Errorf("%s error: %v", p.GetPlatformName(), err)
+				routesChan <- nil
+				return
+			}
+			routesChan <- routes
+			errorsChan <- nil
+		}(platform)
+	}
+
+	// Collect results
+	for i := 0; i < len(pm.platforms); i++ {
+		routes := <-routesChan
+		err := <-errorsChan
+
+		if err != nil {
+			fmt.Printf("Warning: %v\n", err)
+			continue
+		}
+
+		if routes != nil {
+			allRoutes = append(allRoutes, routes...)
+		}
+	}
+
+	return allRoutes, nil
 }
